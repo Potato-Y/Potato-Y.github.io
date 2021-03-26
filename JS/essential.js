8bf0e01e662a47b91cfb0868cc506281910c1207
Potato-Y <projectpotatoy@gmail.com>
2021-03-27 01:56:51 +0900

    Update essential.js

--- JS/essential.js
+++ JS/essential.js
@@ -1,9 +1,8 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
+/* Global site tag (gtag.js) - Google Analytics */
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-WNLMQEDQG9"></script>
-<script>
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-WNLMQEDQG9');
-</script>
