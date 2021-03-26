75e5d4349b74c062e6e6e0a32d5d875988ea896a
Potato-Y <projectpotatoy@gmail.com>
2021-03-27 02:12:44 +0900

    JS코드 분리 취소

--- JS/essential.js
+++
@@ -1,8 +0,0 @@
-/* Global site tag (gtag.js) - Google Analytics */
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WNLMQEDQG9"></script>
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WNLMQEDQG9');
